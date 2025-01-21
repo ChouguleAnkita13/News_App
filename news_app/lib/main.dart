919a90cdc53dce76e892cff446a2b43e13357ec2
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-21 11:45:05 +0530

    commit

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/view/route/app_route.dart'; // Import app routes
-import 'package:news_app/controller/news_provider.dart';
-import 'package:provider/provider.dart';
-import 'package:news_app/theme/app_theme.dart';
+import 'package:news_app/view/route/app_route.dart'; // IMPORT APP ROUTES
+import 'package:news_app/controller/news_provider.dart'; // IMPORT NEWS PROVIDER FOR STATE MANAGEMENT
+import 'package:provider/provider.dart'; // IMPORT PROVIDER PACKAGE FOR STATE MANAGEMENT
+import 'package:news_app/theme/app_theme.dart'; // IMPORT APP THEME
 
+/// MAIN ENTRY POINT OF THE APPLICATION
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // RUN THE APPLICATION
 }
 
+/// ROOT WIDGET OF THE APPLICATION
 class MyApp extends StatelessWidget {
+  /// CONSTRUCTOR FOR MyApp
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /// WRAPPING THE APPLICATION IN A ChangeNotifierProvider TO PROVIDE NewsProvider
     return ChangeNotifierProvider(
-      // Use ChangeNotifierProvider to provide NewsProvider to the entire app
+      /// INITIALIZE NewsProvider FOR THE ENTIRE APP
       create: (context) => NewsProvider(),
       child: MaterialApp(
+        /// SET THE INITIAL SCREEN OF THE APPLICATION
         home: initialScreen,
+
+        /// DEFINE NAMED ROUTES FOR NAVIGATION
         routes: appRoutes,
-        theme: AppTheme.lightTheme, // Apply the theme
-        debugShowCheckedModeBanner: false, // Disable the debug banner
+
+        /// APPLY THE CUSTOM APP THEME
+        theme: AppTheme.lightTheme,
+
+        /// DISABLE THE DEBUG BANNER IN THE APP
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
