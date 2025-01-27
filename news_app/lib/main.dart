b9ca982a1d1431ce97e34658b7354b0e3f7f9a47
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-27 12:06:21 +0530

    firebase connectivity done

--- news_app/lib/main.dart
+++ news_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/view/route/app_route.dart'; // IMPORT APP ROUTES
 import 'package:news_app/controller/news_provider.dart'; // IMPORT NEWS PROVIDER FOR STATE MANAGEMENT
@@ -5,7 +6,14 @@ import 'package:provider/provider.dart'; // IMPORT PROVIDER PACKAGE FOR STATE MA
 import 'package:news_app/theme/app_theme.dart'; // IMPORT APP THEME
 
 /// MAIN ENTRY POINT OF THE APPLICATION
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBhwHkEI7nxQvFWJI6fGaejF02aqKt_BB8",
+          appId: "1:32737209416:android:73ac86769cbf29a338fc7d",
+          messagingSenderId: "32737209416",
+          projectId: "news-app-e7252"));
   runApp(const MyApp()); // RUN THE APPLICATION
 }
 
