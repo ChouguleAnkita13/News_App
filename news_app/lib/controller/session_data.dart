58cb325aa893d26fb651b840c0ae3cf6b577c7b8
ChouguleAnkita13 <flutterankita@gmail.com>
2025-01-30 16:55:20 +0530

    added email and name

--- news_app/lib/controller/session_data.dart
+++ news_app/lib/controller/session_data.dart
@@ -5,13 +5,26 @@ class SessionData {
   /// VARIABLE TO STORE LOGIN STATUS
   static bool? isLogin;
 
-  /// METHOD TO STORE LOGIN STATUS IN SHARED PREFERENCES
-  static Future<void> storeSessionData(bool isLogin) async {
+  /// VARIABLE TO STORE EMAIL
+  static String email = "";
+
+  /// VARIABLE TO STORE USERNAME
+  static String username = "";
+
+  /// METHOD TO STORE LOGIN STATUS,USERNAME AND EMAIL IN SHARED PREFERENCES
+  static Future<void> storeSessionData(
+      bool isLogin, String email, String username) async {
     /// OBTAIN AN INSTANCE OF SHARED PREFERENCES
     final SharedPreferences pref = await SharedPreferences.getInstance();
 
     /// STORE THE LOGIN STATUS USING THE KEY "isLogin"
     pref.setBool("isLogin", isLogin);
+
+    /// STORE THE EMAIL USING THE KEY "email"
+    pref.setString("email", email);
+
+    /// STORE THE EMAIL USING THE KEY "username"
+    pref.setString("username", username);
 
     /// CALL getSessionData TO UPDATE THE STATIC VARIABLE isLogin
     getSessionData();
@@ -24,5 +37,10 @@ class SessionData {
 
     /// RETRIEVE THE VALUE OF "isLogin" OR SET IT TO false IF IT DOES NOT EXIST
     isLogin = pref.getBool("isLogin") ?? false;
+
+    /// RETRIEVE THE VALUE OF "email" AND "username" OR SET IT TO " " IF IT DOES NOT EXIST
+    email = pref.getString("email") ?? "";
+
+    username = pref.getString("username") ?? "";
   }
 }
